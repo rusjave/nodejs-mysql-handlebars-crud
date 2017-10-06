a2a1a6f0238eaeb56499599d5778e3ecd614807b
RussellJaveS.Rivera <russerivera@gmail.com>
2017-10-06 15:48:39 +0800

    Initial Commit

---
+++ routes/account.js
@@ -0,0 +1,111 @@
+var express = require('express');
+var router = express.Router();
+async = require('async');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+	var con = req.con;
+	async.parallel (
+	[
+		function(callback){
+			con.query('SELECT * FROM account', function(errors,accounts){
+				callback(errors, accounts);
+
+			});	
+		}
+	],
+	function(err, results){
+		var data = {accounts: results[0]};
+		res.render('account/index', data);
+		}
+	);  
+});
+
+/* GET add user  page. */
+router.get('/add', function(req, res, next) {
+	res.render('account/add');
+
+});
+
+/* API for add new user*/
+router.post('/add', function(req, res, next) {
+	var con = req.con;
+	async.parallel (
+	[
+		function(callback){
+			con.query('INSERT INTO account(username, password, full_name) values(?,?,?)', [req.body.username, req.body.password, req.body.full_name], 
+				function(errors,accounts){
+					callback(errors);
+			});	
+		}
+	],
+	function(err, results){
+		res.redirect('/account');
+		}
+	);  
+
+});
+
+/* API for delete new user */
+router.get('/delete/:id', function(req, res, next) {
+	var con = req.con;
+	async.parallel (
+	[
+		function(callback){
+			con.query('DELETE FROM account WHERE id = ?', [req.params.id], function(errors,accounts){
+					callback(errors);
+			});	
+		}
+	],
+	function(err, results){
+		res.redirect('/account');
+		}
+	);  
+
+});
+
+/* API for edit new user */
+router.get('/edit/:id', function(req, res, next) {
+	var con = req.con;
+	async.parallel (
+	[
+		function(callback){
+			con.query('SELECT * FROM account WHERE id = ?', [req.params.id], function(errors,accounts){
+					callback(errors, accounts[0]);
+			});	
+		}
+	],
+	function(err, results){
+		var data = {account: results[0]};
+		res.render('account/edit', data);
+		}
+	);  
+
+});
+
+/* API for edit new user */
+router.post('/edit/', function(req, res, next) {
+	var con = req.con;
+	async.parallel (
+	[
+		function(callback){
+			con.query('SELECT * FROM account WHERE id = ?', [req.body.id], function(errors,accounts){
+				var account = accounts[0].password;
+					if(req.body.password !=''){
+						account.password = req.body.password;
+					}
+					con.query('UPDATE  account set username  = ?, password = ?, full_name = ? WHERE id = ?', 
+						[req.body.username, password, req.body.full_name, req.body.id], function(errors,accounts){
+						callback(errors); 
+					});		
+			});	
+		}
+	],
+	function(err, results){
+			res.redirect('/account');
+		}
+	);  
+
+});
+
+module.exports = router;
